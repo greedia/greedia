802707035493b50f1514b054166ded7b0057265a
k3d3 <k@3d3.ca>
2021-01-21 19:31:23 -0500

    Document CacheFileHandler and add seek_to method

--- src/cache_handlers/mod.rs
+++ src/cache_handlers/mod.rs
@@ -29,7 +29,14 @@ trait CacheDriveHandler {
 
 #[async_trait]
 trait CacheFileHandler {
+    /// Read data into `buf`.
     async fn read_into(&mut self, buf: &mut [u8]) -> usize;
+    /// Cache data to file without reading it.
+    async fn cache_data(&mut self, len: usize) -> usize;
+    /// Read data into Bytes object, which may end up less than `len`.
     async fn read_bytes(&mut self, len: usize) -> Bytes;
+    /// Read data into Bytes object which is exactly the size of `len` (unless an EOF occurs).
     async fn read_exact(&mut self, len: usize) -> Bytes;
+    /// Seek stream to `offset`.
+    async fn seek_to(&mut self, offset: u64);
 }
